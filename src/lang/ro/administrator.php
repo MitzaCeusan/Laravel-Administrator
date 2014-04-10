bf4a97a43f12058b89655ca531dd9a78fb3eea56
Mitza Ceusan <mitza.ceusan@gmail.com>
2014-04-10 21:20:19 +0300

    first attempt for romanian translation

---
+++ src/lang/ro/administrator.php
@@ -0,0 +1,55 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| ro - Romanian
+	|
+	*/
+
+	'dashboard' => 'Dashboard',
+	'edit' => 'Editeaza',
+	'filters' => 'Filtre',
+	'loading' => 'Se incarca...',
+	'createnew' => 'Create New',
+	'new' => 'Nou',
+	'viewitem' => 'View :single',
+	'id' => 'ID',
+	'uploadimage' => 'Upload Imagine',
+	'imageuploading' => 'Image Uploading',
+	'uploadfile' => 'Upload Fisier',
+	'fileuploading' => 'File Uploading',
+	'none' => 'Niciunul',
+	'all' => 'Toate',
+	'itemsperpage' => 'inregistrari per pagina',
+	'noresults' => 'Niciun rezultat',
+	'backtosite' => 'Back to Site',
+	'logout' => 'Logout',
+
+	'previous' => 'precedent',
+	'next' => 'urmator',
+
+	'close' => 'Inchide',
+	'delete' => 'Sterge',
+	'save' => 'Salveaza',
+	'create' => 'Creaza',
+	'cancel' => 'Cancel',
+
+	'active' => 'Just a moment...',
+	'success' => 'Succes!',
+	'error' => 'There was an error performing this action',
+
+	'valid_columns' => "You must provide a valid 'columns' array in each model's config",
+	'valid_title' => "You must provide a valid title and single name in each model's config",
+	'valid_model' => "You must provide a 'model' option in each model's config",
+	'valid_edit' => "You must provide a valid 'edit_fields' array in each model's config",
+	'valid_menu' => "You must provide a valid 'menu' option in the administrator.php config",
+	'valid_config_path' => "You must provide a valid 'model_config_path' in the administrator.php config. The directory must also exist and be readable.",
+	'not_eloquent' => " is not an Eloquent model",
+	'storage_path_permissions' => "You must make your storage path writable in order to make a settings page",
+	'valid_home_page' => "You must provide a valid menu item in the home_page option of your config",
+);
---
+++ src/lang/ro/knockout.php
@@ -0,0 +1,24 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines for knockout
+	|--------------------------------------------------------------------------
+	|
+	| ro - Romaniam
+	|
+	*/
+
+	'delete_active_item' => 'Esti sigur ca vrei sa stergi aceasta inregistrare? Actiunea este ireversibila.',
+	'saving' => 'Se salveaza...',
+	'saved' => 'Inregistrare salvata.',
+	'deleting' => 'Se sterge...',
+	'deleted' => 'Inregistrare stearsa.',
+	'character_left' => ' caracter ramas',
+	'characters_left' => ' caractere ramase',
+	'no_results' => 'Niciun rezultat',
+	'select_options' => 'Alege optiuni',
+
+);
